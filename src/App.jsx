0a0b49f9a6ece8234b26f5d54516e1242eadc6e3
yukilzw <yuki.liuzhanwei@gmail.com>
2022-02-14 11:03:10 +0800

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,8 @@
+import Comp from './Comp';
+import './App.css'
+
+function App() {
+  return <Comp />
+}
+
+export default App
---
+++ src/Comp.jsx
@@ -0,0 +1,46 @@
+import { useState } from 'react'
+
+// now, hot reload fail
+const Func = () => 0;
+
+// change to this, hot reload success 👇
+// const func = () => 0;
+
+export default function () {
+  const [count, setCount] = useState(Func)
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>Hello Vite + React!</p>
+        <p>
+          <button type="button" onClick={() => setCount((count) => count + 1)}>
+            count is: {count}
+          </button>
+        </p>
+        <p>
+          Edit <code>App.jsx</code> and save to test HMR updates.
+        </p>
+        <p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+          {' | '}
+          <a
+            className="App-link"
+            href="https://vitejs.dev/guide/features.html"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Vite Docs
+          </a>
+        </p>
+      </header>
+    </div>
+  )
+}
